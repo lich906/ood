3d22ff6475068299633ce2061d9618238973d1eb
AndreyM <222mushkin@gmail.com>
2022-09-28 20:44:26 +0300

    [2-observer] subtask 5: added wind speed and wind direction params

--- 2-observer/WeatherStation/WeatherData.h
+++ 2-observer/WeatherStation/WeatherData.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <climits>
+#include <optional>
 #include "Observer.h"
 
 struct WeatherInfo
@@ -10,6 +11,8 @@ struct WeatherInfo
 	double temperature = 0;
 	double humidity = 0;
 	double pressure = 0;
+	double windSpeed = 0;
+	std::optional<double> windDirection;
 };
 
 class Display: public IObserver<WeatherInfo>
@@ -24,7 +27,23 @@ private:
 		std::cout << "Current Temp " << data.temperature << std::endl;
 		std::cout << "Current Hum " << data.humidity << std::endl;
 		std::cout << "Current Pressure " << data.pressure << std::endl;
+		std::cout << "Current Wind Speed " << data.windSpeed << std::endl;
+		DisplayWindDirection(data.windDirection);
 		std::cout << "----------------" << std::endl;
+	}
+
+	void DisplayWindDirection(const std::optional<double>& windDirection)
+	{
+		if (windDirection.has_value())
+		{
+			std::cout << "Current Wind Direction " << *windDirection;
+		}
+		else
+		{
+			std::cout << "Current Wind Direction -- calm";
+		}
+
+		std::cout << std::endl;
 	}
 };
 
@@ -47,15 +66,19 @@ private:
 		UpdateNumericStatsData(data.temperature, m_temperatureStats);
 		UpdateNumericStatsData(data.humidity, m_humidityStats);
 		UpdateNumericStatsData(data.pressure, m_pressureStats);
+		UpdateNumericStatsData(data.windSpeed, m_windSpeedStats);
+		UpdateAverageWindDirection(data.windDirection);
 
 		++m_count;
 
 		DisplayNumericStatsData(m_temperatureStats, "Temperature");
 		DisplayNumericStatsData(m_humidityStats, "Humidity");
 		DisplayNumericStatsData(m_pressureStats, "Pressure");
+		DisplayNumericStatsData(m_windSpeedStats, "Wind Speed");
+		DisplayAverageWindDirection();
 	}
 
-	void UpdateNumericStatsData(double current, NumericStatsData& statsData)
+	void UpdateNumericStatsData(double current, NumericStatsData& statsData) const
 	{
 		// std::min
 		if (statsData.minimum > current)
@@ -72,7 +95,7 @@ private:
 		statsData.accumulatedValue += current;
 	}
 
-	void DisplayNumericStatsData(const NumericStatsData& statsData, const char* text)
+	void DisplayNumericStatsData(const NumericStatsData& statsData, const char* text) const
 	{
 		std::cout << "Max " << text << " " << statsData.maximum << std::endl;
 		std::cout << "Min " << text << " " << statsData.minimum << std::endl;
@@ -80,9 +103,63 @@ private:
 		std::cout << "----------------" << std::endl;
 	}
 
+	void UpdateAverageWindDirection(const std::optional<double>& current)
+	{
+		if (current.has_value())
+		{
+			if (m_avgWindDirection.has_value())
+			{
+				double mid;
+				if ((mid = abs(*current - *m_avgWindDirection)) > 180)
+				{
+					double average = (*current + *m_avgWindDirection) / 2;
+					if (average > 180)
+					{
+						m_avgWindDirection = average - 180;
+					}
+					else if (average < 180)
+					{
+						m_avgWindDirection = average + 180;
+					}
+					else
+					{
+						m_avgWindDirection = std::nullopt;
+					}
+				}
+				else if (mid < 180)
+				{
+					m_avgWindDirection = (*current + *m_avgWindDirection) / 2;
+				}
+				else
+				{
+					m_avgWindDirection = std::nullopt;
+				}
+			}
+			else
+			{
+				m_avgWindDirection = current;
+			}
+		}
+	}
+
+	void DisplayAverageWindDirection()
+	{
+		if (m_avgWindDirection.has_value())
+		{
+			std::cout << "Avg wind direction " << *m_avgWindDirection << std::endl;
+		}
+		else
+		{
+			std::cout << "Avg wind direction -- calm" << std::endl;
+		}
+	}
+
 	NumericStatsData m_temperatureStats;
 	NumericStatsData m_humidityStats;
 	NumericStatsData m_pressureStats;
+	NumericStatsData m_windSpeedStats;
+
+	std::optional<double> m_avgWindDirection;
 
 	unsigned m_count = 0;
 };
@@ -106,16 +183,28 @@ public:
 		return m_pressure;
 	}
 
+	double GetWindSpeed() const
+	{
+		return m_windSpeed;
+	}
+
+	double GetWindDirection() const
+	{
+		return m_windDirection;
+	}
+
 	void MeasurementsChanged()
 	{
 		NotifyObservers();
 	}
 
-	void SetMeasurements(double temp, double humidity, double pressure)
+	void SetMeasurements(double temp, double humidity, double pressure, double windSpeed, double windDirection)
 	{
 		m_humidity = humidity;
 		m_temperature = temp;
 		m_pressure = pressure;
+		m_windSpeed = windSpeed;
+		m_windDirection = windDirection;
 
 		MeasurementsChanged();
 	}
@@ -126,10 +215,14 @@ protected:
 		info.temperature = GetTemperature();
 		info.humidity = GetHumidity();
 		info.pressure = GetPressure();
+		info.windSpeed = GetWindSpeed();
+		info.windDirection = GetWindDirection();
 		return info;
 	}
 private:
 	double m_temperature = 0.0;
-	double m_humidity = 0.0;	
-	double m_pressure = 760.0;	
+	double m_humidity = 0.0;
+	double m_pressure = 760.0;
+	double m_windSpeed = 0.0;
+	double m_windDirection = 0.0;
 };
--- 2-observer/WeatherStation/main.cpp
+++ 2-observer/WeatherStation/main.cpp
@@ -10,13 +10,13 @@ int main()
 	StatsDisplay statsDisplay;
 	wd.RegisterObserver(statsDisplay);
 
-	wd.SetMeasurements(3, 0.7, 760);
-	wd.SetMeasurements(4, 0.8, 761);
+	wd.SetMeasurements(3, 0.7, 760, 3, 90);
+	wd.SetMeasurements(4, 0.8, 761, 5, 270);
 
-	wd.RemoveObserver(statsDisplay);
+	//wd.RemoveObserver(statsDisplay);
 	std::cout << "===========================" << std::endl;
 
-	wd.SetMeasurements(10, 0.8, 761);
-	wd.SetMeasurements(-10, 0.8, 761);
+	wd.SetMeasurements(10, 0.8, 761, 7, 350);
+	wd.SetMeasurements(-10, 0.8, 761, 8, 90);
 	return 0;
 }
