7de5b305265e8a813cd0b853a7c958311247774f
AndreyM <222mushkin@gmail.com>
2022-12-17 19:14:41 +0300

    [8-state] added console menu

--- 8-state/multi_gumball_machine_refillable/main.cpp
+++ 8-state/multi_gumball_machine_refillable/main.cpp
@@ -1,51 +1,28 @@
+#include "MenuFP.h"
 #include "GumballMachine.h"
-
-using namespace std;
-
-template <typename GumballMachineType>
-void TestGumballMachine(GumballMachineType& m)
-{
-	cout << m.ToString() << endl;
-
-	m.InsertQuarter();
-	m.TurnCrank();
-
-	cout << m.ToString() << endl;
-
-	m.InsertQuarter();
-	m.EjectQuarter();
-	m.TurnCrank();
-
-	cout << m.ToString() << endl;
-
-	m.InsertQuarter();
-	m.TurnCrank();
-	m.InsertQuarter();
-	m.TurnCrank();
-	m.EjectQuarter();
-
-	cout << m.ToString() << endl;
-
-	m.InsertQuarter();
-	m.InsertQuarter();
-	m.TurnCrank();
-	m.InsertQuarter();
-	m.TurnCrank();
-	m.InsertQuarter();
-	m.TurnCrank();
-
-	cout << m.ToString() << endl;
-}
-
-void TestGumballMachineWithState()
-{
-	GumballMachine m(5);
-	TestGumballMachine(m);
-}
 
 int main()
 {
-	TestGumballMachineWithState();
+	GumballMachine machine(0);
+
+	CMenuFP menu;
+
+	menu.AddItem("info", "prints info about gumball machine", [&machine]() {
+		std::cout << machine.ToString();
+	});
+	menu.AddItem("insert-quarter", "insert quarter in gumball machine", std::bind(&GumballMachine::InsertQuarter, &machine));
+	menu.AddItem("eject-quarter", "eject all quarters from gumball machine", std::bind(&GumballMachine::EjectQuarter, &machine));
+	menu.AddItem("turn-crank", "turn crank and possibly get a gumball", std::bind(&GumballMachine::TurnCrank, &machine));
+	menu.AddItem("refill", "refill machine with gumballs or empty it", [&machine]() {
+		std::cout << "How many gumballs you want to load to machine? (postive integer or 0 to empty)\n>";
+		std::string str;
+		std::getline(std::cin, str);
+		machine.Refill(std::stoul(str));
+	});
+	menu.AddItem("exit", "exit current program", std::bind(&CMenuFP::Exit, &menu));
+	menu.AddItem("help", "prints help message", std::bind(&CMenuFP::ShowInstructions, &menu));
+
+	menu.Run();
 
 	return 0;
 }
